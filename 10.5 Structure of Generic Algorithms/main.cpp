7ad79611861718313bc2fba757a09bff55be4bb7
GHBCL <a1379191885@gmail.com>
2023-03-15 20:50:38 +0800

    10.5

---
+++ 10.5 Structure of Generic Algorithms/main.cpp
@@ -0,0 +1,16 @@
+#include <iterator>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+int main()
+{
+	//10.5.1 The Five Iterator Categories
+	// Input iterator //Read, but not write; single-pass, increment only
+	//Output iterator //Write, but not read; single-pass, increment only
+	//Forward iterator //Read and write; multi-pass, increment only
+	//Bidirectional iterator  //Read and write; multi-pass, increment and decrement
+	//Random-access iterator //Read and write; multi-pass, full iterator arithmetic
+
+}
